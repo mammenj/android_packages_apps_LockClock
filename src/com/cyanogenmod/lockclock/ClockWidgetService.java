59addb678928794386da703bca0f7756fdc1f92c
jsm <mammenj@gmail.com>
2013-03-04 10:37:38 +0530

    FIX: do not display event too far in the future This patch add a filter to drop events after the "lookahead" value (in local time). Add a comment line to clarify why we have to fetch event farther in the futur for "all day" events.

--- src/com/cyanogenmod/lockclock/ClockWidgetService.java
+++ src/com/cyanogenmod/lockclock/ClockWidgetService.java
@@ -521,7 +521,8 @@ public class ClockWidgetService extends IntentService {
 		int DESCRIPTION_INDEX = 4;
 		int LOCATION_INDEX = 5;
 		int ALL_DAY_INDEX = 6;
-
+		
+		// all day events are stored in UTC, that is why we need to fetch events after 'later'
 		Uri uri = Uri.withAppendedPath(
 				CalendarContract.Instances.CONTENT_URI,
 				String.format("%d/%d", now - DAY_IN_MILLIS, later
@@ -563,7 +564,7 @@ public class ClockWidgetService extends IntentService {
 						end = convertUtcToLocal(time, end);
 					}
 
-					if (end < now) {
+					if (end < now || begin > later) {
 						continue;
 					}
 
