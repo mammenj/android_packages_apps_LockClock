cbe78afbd357f92e534471e9b6e16ac5d0b6b1ee
mammenj <mammenj@gmail.com>
2013-03-07 09:35:31 +0530

    code clean up

--- src/com/cyanogenmod/lockclock/preference/Preferences.java
+++ src/com/cyanogenmod/lockclock/preference/Preferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2012 The CyanogenMod Project (DvTonder)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,230 +16,110 @@
 
 package com.cyanogenmod.lockclock.preference;
 
+import java.util.List;
+
 import android.appwidget.AppWidgetManager;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.content.Intent;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.TextView;
 
 import com.cyanogenmod.jsm.cmclockmod.R;
-import java.util.List;
 
 public class Preferences extends PreferenceActivity {
 
-    // only used when adding a new widget
-    private int mNewWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
-	
-    @Override
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.preferences_headers, target);
-        updateHeaders(target);
+	// only used when adding a new widget
+	private int mNewWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 
-        // Check if triggered from adding a new widget
-        Intent intent = getIntent();
-        if (intent != null
-                && AppWidgetManager.ACTION_APPWIDGET_CONFIGURE.equals(intent.getAction())) {
-            mNewWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mNewWidgetId);
-            // See http://code.google.com/p/android/issues/detail?id=2539
-            myResult(RESULT_CANCELED);
-        }
-    }
+	@Override
+	public void onBuildHeaders(List<Header> target) {
+		loadHeadersFromResource(R.xml.preferences_headers, target);
+		updateHeaders(target);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-        return true;
-    }
+		// Check if triggered from adding a new widget
+		Intent intent = getIntent();
+		if (intent != null
+				&& AppWidgetManager.ACTION_APPWIDGET_CONFIGURE.equals(intent
+						.getAction())) {
+			mNewWidgetId = intent.getIntExtra(
+					AppWidgetManager.EXTRA_APPWIDGET_ID, mNewWidgetId);
+			// See http://code.google.com/p/android/issues/detail?id=2539
+			myResult(RESULT_CANCELED);
+		}
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.menu_done:
-                myResult(RESULT_OK);
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
 
-    @Override
-    public void onBackPressed() {
-        // If launched from the configure intent, signal RESULT_OK
-        myResult(RESULT_OK);
-        super.onBackPressed();
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_done:
+			myResult(RESULT_OK);
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
-    private void myResult(int result) {
-        if (mNewWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
-            setResult(result, new Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mNewWidgetId));
-        }
-    }
+	@Override
+	public void onBackPressed() {
+		// If launched from the configure intent, signal RESULT_OK
+		myResult(RESULT_OK);
+		super.onBackPressed();
+	}
 
-    private void updateHeaders(List<Header> headers) {
-        int i = 0;
-        
-        while (i < headers.size()) {
-            Header header = headers.get(i);
-            i++;
-        }
-    }
+	private void myResult(int result) {
+		if (mNewWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+			setResult(result, new Intent().putExtra(
+					AppWidgetManager.EXTRA_APPWIDGET_ID, mNewWidgetId));
+		}
+	}
 
-    @Override
-    public void setListAdapter(ListAdapter adapter) {
-        if (adapter == null) {
-            super.setListAdapter(null);
-        } else {
-            //super.setListAdapter(new HeaderAdapter(this, getHeaders()));
-            super.setListAdapter(adapter);
-        }
-    }
+	private void updateHeaders(List<Header> headers) {
+		int i = 0;
 
-    public static class ClockFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+		while (i < headers.size()) {
+			Header header = headers.get(i);
+			i++;
+		}
+	}
 
-            addPreferencesFromResource(R.xml.preferences_clock);
+	public static class ClockFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
 
-        }
-    }
+			addPreferencesFromResource(R.xml.preferences_clock);
 
-    public static class WeatherFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+		}
+	}
 
-            addPreferencesFromResource(R.xml.preferences_weather);
-        }
-    }
+	public static class WeatherFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
 
-    public static class CalendarFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.preferences_weather);
+		}
+	}
 
-            addPreferencesFromResource(R.xml.preferences_calendar);
-        }
-    }
+	public static class CalendarFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
 
-    private static class HeaderAdapter extends ArrayAdapter<Header> {
-        private static final int HEADER_TYPE_NORMAL = 0;
-        private static final int HEADER_TYPE_CATEGORY = 1;
+			addPreferencesFromResource(R.xml.preferences_calendar);
+		}
+	}
 
-        private static final int HEADER_TYPE_COUNT = HEADER_TYPE_CATEGORY + 1;
-
-        private static class HeaderViewHolder {
-            ImageView icon;
-            TextView title;
-            TextView summary;
-        }
-
-        private LayoutInflater mInflater;
-
-        static int getHeaderType(Header header) {
-            return HEADER_TYPE_NORMAL;
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            Header header = getItem(position);
-            return getHeaderType(header);
-        }
-
-        @Override
-        public boolean areAllItemsEnabled() {
-            return false; // because of categories
-        }
-
-        @Override
-        public boolean isEnabled(int position) {
-            return getItemViewType(position) != HEADER_TYPE_CATEGORY;
-        }
-
-        @Override
-        public int getViewTypeCount() {
-            return HEADER_TYPE_COUNT;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        public HeaderAdapter(Context context, List<Header> objects) {
-            super(context, 0, objects);
-
-            mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            HeaderViewHolder holder;
-            Header header = getItem(position);
-            int headerType = getHeaderType(header);
-            View view = null;
-
-            if (convertView == null) {
-                holder = new HeaderViewHolder();
-                switch (headerType) {
-                    case HEADER_TYPE_CATEGORY:
-                        view = new TextView(getContext(), null,
-                                android.R.attr.listSeparatorTextViewStyle);
-                        holder.title = (TextView) view;
-                        break;
-
-                    case HEADER_TYPE_NORMAL:
-                        view = mInflater.inflate(
-                                R.layout.preference_header_item, parent,
-                                false);
-                        holder.icon = (ImageView) view.findViewById(R.id.icon);
-                        holder.title = (TextView)
-                                view.findViewById(R.id.title);
-                        holder.summary = (TextView)
-                                view.findViewById(R.id.summary);
-                        break;
-                }
-                view.setTag(holder);
-            } else {
-                view = convertView;
-                holder = (HeaderViewHolder) view.getTag();
-            }
-
-            // All view fields must be updated every time, because the view may be recycled
-            switch (headerType) {
-                case HEADER_TYPE_CATEGORY:
-                    holder.title.setText(header.getTitle(getContext().getResources()));
-                    break;
-
-                case HEADER_TYPE_NORMAL:
-                    holder.icon.setImageResource(header.iconRes);
-                    holder.title.setText(header.getTitle(getContext().getResources()));
-                    CharSequence summary = header.getSummary(getContext().getResources());
-                    if (!TextUtils.isEmpty(summary)) {
-                        holder.summary.setVisibility(View.VISIBLE);
-                        holder.summary.setText(summary);
-                    } else {
-                        holder.summary.setVisibility(View.GONE);
-                    }
-                    break;
-            }
-
-            return view;
-        }
-    }
 }
